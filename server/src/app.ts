f6469bac049490242bacce467740cda6651dcbd6
Sajidur Rahman Siam <siamsajidur@gmail.com>
2026-02-04 03:58:02 +0600

    User Module Added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,27 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import express from "express";
+import { UserRoutes } from "./app/modules/user/user.route";
+import cors from "cors";
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 
 const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", router)
 
 app.get("/", (req:Request , res: Response)=>{
     res.status(200).json({
         message : "Welcome to Tour Management System!!"
 })
 })
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
 
 export default app;
